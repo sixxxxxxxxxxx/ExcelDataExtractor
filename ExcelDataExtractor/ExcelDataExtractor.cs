ca8c44df25f71ffebc382c317cbfe07c574cf503
six <jcuudeh@gmail.com>
2022-07-08 11:59:37 +0100

    Initial commit

---
+++ ExcelDataExtractor/ExcelDataExtractor.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using OfficeOpenXml;
+
+namespace ExcelDataExtractor
+{
+    public static class ExcelDataExtractor
+    {
+        public static IEnumerable<T> ReadFromExcel<T>(IFormFile excelFile, string[]? requiredHeaders, string[]? nullableFields, string[]? ignoreFields, string duplicateComparer) where T : new()
+        {
+            List<Dictionary<string, string>> excelData = ExcelDataExtractorExtension.ExtractFromExcel(excelFile, nullableFields, ignoreFields);
+
+            if (requiredHeaders != null)
+            {
+                excelData.ValidateFields(requiredHeaders);
+            }
+            
+            excelData.CheckDuplicate(duplicateComparer);
+
+            IEnumerable<T> recordsToUpload = DictionaryToObjectConverterExtension.DictionaryToObjects<T>(excelData);
+            return recordsToUpload;
+        }
+
+        public static MemoryStream WriteToExcel<T>(this IList<T> collection, string worksheetName, int[] unlockedColumns, int[] hiddenColumns)
+        {
+            var stream = new MemoryStream();
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (var ep = new ExcelPackage(stream))
+            {
+                ExcelWorksheet worksheet = ep.Workbook.Worksheets.Add(worksheetName);
+                worksheet.Cells.LoadFromCollection(collection, true);
+
+                worksheet.Protection.IsProtected = true;
+
+                if (unlockedColumns != null && unlockedColumns.Any())
+                {
+                    foreach (var column in unlockedColumns)
+                        worksheet.Column(column).Style.Locked = false;
+                }
+
+                if (hiddenColumns != null && hiddenColumns.Any())
+                {
+                    foreach (var column in hiddenColumns)
+                        worksheet.Column(column).Hidden = true;
+                }
+
+                worksheet.Cells[worksheet.Dimension.Address].AutoFitColumns();
+                ep.Save();
+            }
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}
---
+++ ExcelDataExtractor/ExcelDataExtractorExtension.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Http;
+using OfficeOpenXml;
+
+namespace ExcelDataExtractor
+{
+    public static class ExcelDataExtractorExtension
+    {
+        public static List<Dictionary<string, string>> ExtractFromExcel(IFormFile file, string[]? nullableFields, string[]? ignoreFields)
+        {
+            int headerRow = 1;
+            int contentRow = 2;
+
+            if (file == null)
+            {
+                throw new InvalidOperationException("File is empty");
+            }
+
+            if (!Path.GetExtension(file.FileName).Equals(".xlsx", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new InvalidDataException("Incorrect file format");
+            }
+
+            List<Dictionary<string, string>> excelData = new();
+
+            List<string> possibleEmptyFields = new();
+
+
+            if (nullableFields != null)
+                possibleEmptyFields.AddRange(nullableFields);
+
+            possibleEmptyFields = possibleEmptyFields.Select(c => c.ToLower()).ToList();
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using MemoryStream stream = new();
+            file.CopyTo(stream);
+            using ExcelPackage ep = new(stream);
+            ExcelWorksheet worksheet = ep.Workbook.Worksheets.First();
+            int rowCount = worksheet.Dimension.Rows;
+            int columnCount = worksheet.Dimension.Columns;
+            nullableFields = nullableFields?.Select(c => c.ToLower()).ToArray();
+            ignoreFields = ignoreFields?.Select(c => c.ToLower()).ToArray();
+
+            if (nullableFields != null)
+                worksheet.Validate(nullableFields, columnCount);
+
+            if (ignoreFields != null)
+                worksheet.Validate(ignoreFields, columnCount);
+
+
+            for (int row = contentRow; row <= rowCount; row++)
+            {
+                Dictionary<string, string> cell = new();
+                for (int column = 1; column <= columnCount; column++)
+                {
+                    string headerCell = worksheet.Cells[headerRow, column].Value.ToString();
+
+                    if (ignoreFields != null && ignoreFields.Contains(headerCell?.ToLower()))
+                        continue;
+
+                    string value;
+                    if (possibleEmptyFields.Any(l => !string.IsNullOrWhiteSpace(headerCell) && l == headerCell.ToLower()))
+                    {
+                        value = worksheet.Cells[row, column].Value?.ToString();
+                    }
+                    else if (worksheet.Cells[row, column].Value == null)
+                    {
+                        throw new InvalidDataException("Excel has empty fields. Crosscheck it and submit again");
+                    }
+                    else
+                    {
+                        value = worksheet.Cells[row, column].Value.ToString();
+                    }
+
+                    if (headerCell != null)
+                        cell.Add(headerCell, value);
+                }
+
+                excelData.Add(cell);
+            }
+            ep.Save();
+            return excelData;
+        }
+
+        public static void Validate(this ExcelWorksheet worksheet, string[] fields, int columnCount)
+        {
+            if (fields == null) return;
+            foreach (var field in fields)
+            {
+                if (!Headers(worksheet, columnCount).Contains(field))
+                    throw new InvalidDataException($"{field} Field Doesn't Exist");
+            }
+        }
+        public static void ValidateFields(this List<Dictionary<string, string>> source, string[] fields)
+        {
+            var equal = source.FirstOrDefault()?.Select(c => c.Key).OrderBy(c => c, StringComparer.OrdinalIgnoreCase)
+                .SequenceEqual(fields.Select(c => c).OrderBy(c => c, StringComparer.OrdinalIgnoreCase));
+
+            if (equal != null && !equal.Value)
+                throw new InvalidOperationException("The excel sheet uploaded is not for this purpose, crosscheck your headers or download the sample excel provided");
+        }
+
+        public static void CheckDuplicate(this List<Dictionary<string, string>> source, string column)
+        {
+            List<string> list = new List<string>();
+
+            foreach (var row in source)
+            {
+                list.Add(row[column]);
+            }
+
+            if (list.GroupBy(x => x).Any(g => g.Count() > 1))
+            {
+                throw new InvalidOperationException("Data contains duplicate records");
+            }
+        }
+
+        private static List<string> Headers(ExcelWorksheet worksheet, int columnCount)
+        {
+            List<string> headers = new List<string>(columnCount);
+            for (int column = 1; column <= columnCount; column++)
+            {
+                var header = worksheet.Cells[1, column].Value.ToString()?.ToLower();
+                headers.Add(header);
+            }
+            return headers;
+        }
+    }
+}
